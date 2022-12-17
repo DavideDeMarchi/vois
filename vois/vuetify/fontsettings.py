ee2db799ad84bfdc509714bb672a26ce15c4d094
Davide De Marchi <davide.de-marchi@ec.europa.eu>
2022-12-17 09:46:06 +0100

    Small changes

--- vois/vuetify/fontsettings.py
+++ vois/vuetify/fontsettings.py
@@ -21,5 +21,3 @@
 # Font name and URL
 font_name = 'Roboto'
 font_url  = 'https://fonts.googleapis.com/css?family=%s:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' % (font_name)
-
-jeodpp_roboto_url  = 'https://jeodpp.jrc.ec.europa.eu/services/shared/Notebooks/fonts/Roboto-Regular.ttf'
