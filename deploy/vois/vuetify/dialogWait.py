7e31f95c4ed5218f7c715f73370d29316b89a971
Davide De Marchi <davide.de-marchi@ec.europa.eu>
2023-04-13 19:45:08 +0200

    Added other parameters to dialogWait

--- deploy/vois/vuetify/dialogWait.py
+++ deploy/vois/vuetify/dialogWait.py
@@ -53,6 +53,14 @@ class dialogWait(v.VuetifyTemplate):
     indeterminate : bool, optional
         If True the progress bar will constantly animate (to be used when completion progress is unknown). Default is True.
         If set to False, setting the value property of the dialog (e.g.: dlg.value = 30) to the percentage will change the progress bar.
+    height : int, optional
+        Height of the progress bar in pixels (default is 4)
+    linecolor : str, optional
+        Color of the progress bar (default is None, meaning that the progress bar will be white or black depending on settings.dark_mode)
+    showtext : bool, optional
+        If True the percentage text will be displayed at the center of the progress bar (default is False)
+    textcolor : str, optional
+        Color of the percentage text displayed if the showtext parameter is True (default is '#cccccc')
     output : ipywidgets.Output, optional
         Output widget on which the widget has to be displayed
             
@@ -81,14 +89,30 @@ class dialogWait(v.VuetifyTemplate):
     text          = traitlets.Unicode('').tag(sync=True)
     indeterminate = traitlets.Bool(True).tag(sync=True)
     value         = traitlets.Int(40).tag(sync=True)
+    height        = traitlets.Int(5).tag(sync=True)
+    showtext      = traitlets.Bool(False).tag(sync=True)
+    textcolor     = traitlets.Unicode('#cccccc').tag(sync=True)
     
     @traitlets.default('template')
     def _template(self):
         darkmode  = ''
-        linecolor = settings.textcolor_notdark
         if settings.dark_mode:
             darkmode  = 'dark'
-            linecolor = settings.textcolor_dark
+            
+        if not self.linecolor is None:
+            linecolor = self.linecolor
+        else:
+            linecolor = settings.textcolor_notdark
+            if settings.dark_mode:
+                linecolor = settings.textcolor_dark
+
+        sss = ''
+        if self.showtext:
+            sss = '''
+<template v-slot:default="{ value }">
+   <strong style="color: %s;">{{ Math.ceil(value) }}%%</strong>
+</template>
+''' % self.textcolor
     
         return '''
 <template>
@@ -109,21 +133,27 @@ class dialogWait(v.VuetifyTemplate):
           <v-progress-linear
             :indeterminate="indeterminate"
             :value="value"
+            :height="height"
             color="%s"
-            class="mb-0 mt-2"
-          ></v-progress-linear>
+            class="mb-0 mt-2">
+            %s
+          </v-progress-linear>
         </v-card-text>
       </v-card>
     </v-dialog>
   </div>
-</template>''' % (settings.color_first, darkmode, linecolor)
+</template>''' % (settings.color_first, darkmode, linecolor, sss)
     
     
-    def __init__(self, output=None, text='Please wait...', indeterminate=True, *args, **kwargs):
+    def __init__(self, output=None, text='Please wait...', indeterminate=True, height=4, linecolor=None, showtext=False, textcolor='#cccccc', *args, **kwargs):
         
-        self.text = text
+        self.text          = text
         self.indeterminate = indeterminate
-        self.value = 0
+        self.height        = height
+        self.linecolor     = linecolor
+        self.showtext      = showtext
+        self.textcolor     = textcolor
+        self.value         = 0
         super().__init__(*args, **kwargs)
         
         if not output is None:
