dd6e8d7fb12fd155176c7688a33d1a8c3a220f8f
Davide De Marchi <davide.de-marchi@ec.europa.eu>
2023-06-12 07:18:35 +0200

    Fixed help after src folder adding

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../vois'))
-sys.path.insert(0, os.path.abspath('../vois/vuetify'))
+sys.path.insert(0, os.path.abspath('../src/vois'))
+sys.path.insert(0, os.path.abspath('../src/vois/vuetify'))
 sys.path.insert(0, os.path.abspath('./_templates'))
 
 
