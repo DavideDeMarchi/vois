9749e63e1c979818b11b977a7159e4b971b977fe
Davide De Marchi <davide.de-marchi@ec.europa.eu>
2023-11-02 11:14:09 +0100

    Updated docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,8 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.autosummary',
               'sphinx.ext.napoleon',
-              'sphinx_copybutton'
+              'sphinx_copybutton',
+              'sphinx_rtd_theme',
               #'nbsphinx',
               #'sphinxcontrib.bibtex',
 ]
@@ -91,18 +92,18 @@ numfig = True
 #
 #html_theme = 'alabaster'
 #import sphinx_rtd_theme
-#html_theme = "sphinx_rtd_theme"
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_show_sourcelink = False
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-#html_theme_options = {
-#    'logo_only': True,
-#    
-#}
+html_theme_options = {
+    'logo_only': True,
+    
+}
 
 html_logo = 'figures/vois_horizontal_small_white.png'
 
