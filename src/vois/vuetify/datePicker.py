9f269a67382e567f230f1ab03de7a4b3ccc9649b
Edoardo Ramalli <Edoardo.RAMALLI@eu.europa.eu>
2024-08-26 12:34:44 +0200

    Tests datePicker

--- src/vois/vuetify/datePicker.py
+++ src/vois/vuetify/datePicker.py
@@ -53,9 +53,9 @@ class datePicker():
     disabled : bool, optional
         True if the selection of the date is disabled, False otherwise (default is False)
     mindate : str, optional
-        Minimum selectable date (default is None)
+        Minimum selectable date in format 'YYYY-MM-DD' (default is None)
     maxdate : str, optional
-        Maximum selectable date (default is None)
+        Maximum selectable date in format 'YYYY-MM-DD' (default is None)
 
     Example
     -------
@@ -84,65 +84,76 @@ class datePicker():
 
        Example of a datePicker
     """
-    
-    def __init__(self, date=None, label='', dark=settings.dark_mode, width=88, color=settings.color_first,
-                       show_week=False, onchange=None, offset_x=False, offset_y=True,
-                       disabled=False, mindate=None, maxdate=None):
-        
+
+    def __init__(self,
+                 date=None,
+                 label='',
+                 dark=settings.dark_mode,
+                 width=88,
+                 color=settings.color_first,
+                 show_week=False,
+                 onchange=None,
+                 offset_x=False,
+                 offset_y=True,
+                 disabled=False,
+                 mindate=None,
+                 maxdate=None):
+
         if date is None:
             self._date = datetime.today().strftime('%Y-%m-%d')
         else:
             self._date = date
 
-        self.label      = label
-        self.dark       = dark
-        self.width      = width
-        self.color      = color
-        self.show_week  = show_week
-        self.onchange   = onchange
-        self.offset_x   = offset_x
-        self.offset_y   = offset_y
-        self._disabled  = disabled
-        self.mindate    = mindate
-        self.maxdate    = maxdate
+        self.label = label
+        self.dark = dark
+        self.width = width
+        self.color = color
+        self.show_week = show_week
+        self.onchange = onchange
+        self.offset_x = offset_x
+        self.offset_y = offset_y
+        self._disabled = disabled
+        self.mindate = mindate
+        self.maxdate = maxdate
 
         margins = "mb-n5"
         if len(self.label) == 0: margins = "mb-n5 mt-n2"
-            
-        
-        if self._disabled: von = ''
-        else:              von = 'menuData.on'
-        self.tf = v.TextField(v_on=von, style_="max-width: %dpx; overflow: hidden;"%self.width, v_model=self._date,
+
+        if self._disabled:
+            von = ''
+        else:
+            von = 'menuData.on'
+        self.tf = v.TextField(v_on=von, style_="max-width: %dpx; overflow: hidden;" % self.width, v_model=self._date,
                               type_="date", color=self.color, readonly=True, label=self.label, class_=margins)
 
-        self.ctf = v.Card(flat=True, style_="max-width: %dpx; overflow: hidden;"%self.width, class_="pa-0 ma-0", children=[self.tf])
+        self.ctf = v.Card(flat=True, style_="max-width: %dpx; overflow: hidden;" % self.width, class_="pa-0 ma-0",
+                          children=[self.tf])
 
         self.p = v.DatePicker(v_model=self._date, flat=True, elevation=0, color=self.color, header_color=self.color,
                               style_="max-width: 290px;", show_week=self.show_week, dark=self.dark)
-        
+
         if not self.mindate is None: self.p.min = self.mindate
         if not self.maxdate is None: self.p.max = self.maxdate
-            
+
         self.p.on_event('input', self.__internal_onchange)
 
-        self.m = v.Menu(offset_x=self.offset_x, offset_y=self.offset_y, open_on_hover=False, dense=True, close_on_click=True, close_on_content_click=False,
+        self.m = v.Menu(offset_x=self.offset_x, offset_y=self.offset_y, open_on_hover=False, dense=True,
+                        close_on_click=True, close_on_content_click=False,
                         v_slots=[{'name': 'activator', 'variable': 'menuData', 'children': [self.ctf]}],
-                        children=[self.p] )
+                        children=[self.p])
 
-        
     # Manage 'input' event
     def __internal_onchange(self, widget, event, data):
         self._date = data
         self.tf.v_model = data
         if self.onchange:
             self.onchange()
-        
+
     # Returns the vuetify object to display (the v.Menu)
     def draw(self):
         """Returns the ipyvuetify object to display (the internal v.Menu)"""
         return self.m
 
-    
     # date property
     @property
     def date(self):
@@ -164,21 +175,19 @@ class datePicker():
         """
         return self._date
 
-    
     @date.setter
     def date(self, d):
         if isinstance(d, str) and len(d) == 10 and d[4] == '-' and d[7] == '-':
             self._date = d
             self.tf.v_model = self._date
-            self.p.v_model  = self._date
+            self.p.v_model = self._date
             if self.onchange:
                 self.onchange()
         else:
             self._date = None
             self.tf.v_model = self._date
-            self.p.v_model  = self._date
-            
-            
+            self.p.v_model = self._date
+
     # disabled property
     @property
     def disabled(self):
@@ -200,10 +209,11 @@ class datePicker():
         """
         return self._disabled
 
-    
     @disabled.setter
     def disabled(self, flag):
         self._disabled = bool(flag)
-        if self._disabled: von = ''
-        else:              von = 'menuData.on'
+        if self._disabled:
+            von = ''
+        else:
+            von = 'menuData.on'
         self.tf.v_on = von
