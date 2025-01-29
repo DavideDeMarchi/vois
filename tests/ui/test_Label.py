a91fe73f40c7e427f308b4e7753a7a4ddf924aa6
Edoardo Ramalli <Edoardo.RAMALLI@ec.europa.eu>
2025-01-29 11:47:28 +0100

    Refactoring Label

--- tests/ui/test_Label.py
+++ tests/ui/test_Label.py
@@ -85,7 +85,7 @@ class Test_label:
         # assert_vois_compare_image(image=canvas_2_locator.screenshot(animations='disabled'), postfix='2_canvas')
 
     def test_click(self, ipywidgets_vois_runner, page_session, assert_vois_compare_image,
-                         assert_vois_bytes_image):
+                   assert_vois_bytes_image):
         def kernel_code():
             import sys
             sys.path.append('/Users/edoardo/JRC_Projects/vois/src/')
@@ -111,14 +111,105 @@ class Test_label:
 
         # TODO the onclick function require already to do the redraw
 
-        # ipywidgets_vois_runner(kernel_code)
-        #
-        # my_label_1_locator = page_session.get_by_text("Label 1 Before", exact=False)
-        # my_label_1_locator.wait_for()
-        #
-        # assert_vois_compare_image(image=my_label_1_locator.screenshot(animations='disabled'), postfix='before')
-        #
-        # my_label_1_locator.click()
-        #
-        # assert_vois_compare_image(image=my_label_1_locator.screenshot(animations='disabled'), postfix='after')
+        ipywidgets_vois_runner(kernel_code)
 
+        my_label_1_locator = page_session.get_by_text("Label 1 Before", exact=False)
+        my_label_1_locator.wait_for()
+
+        assert_vois_compare_image(image=my_label_1_locator.screenshot(animations='disabled'), postfix='before')
+
+        my_label_1_locator.click()
+
+        my_label_after_locator = page_session.get_by_text("Label 1 After ciao", exact=False)
+        my_label_after_locator.wait_for()
+
+        assert_vois_compare_image(image=my_label_after_locator.screenshot(animations='disabled'), postfix='after')
+
+    def test_tooltip(self, ipywidgets_vois_runner, page_session, assert_vois_compare_image,
+                   assert_vois_bytes_image):
+        pass
+        # TODO
+
+
+class Test_Label:
+
+    def test_simple_init(self, ipywidgets_vois_runner, page_session, assert_vois_compare_image,
+                         assert_vois_bytes_image):
+        def kernel_code():
+            import sys
+            sys.path.append('/Users/edoardo/JRC_Projects/vois/src/')
+
+            import warnings
+            warnings.filterwarnings("ignore")
+
+            from vois.vuetify import Label, settings
+
+            l1 = Label(text='Label 1 test long long long long long label',
+                       on_click=None,
+                       argument=None,
+                       disabled=False,
+                       text_weight=350,
+                       height=20,
+                       margins=0,
+                       margin_top=None,
+                       icon=None,
+                       icon_large=False,
+                       icon_small=False,
+                       icon_left=False,
+                       icon_color='black',
+                       tooltip=None,
+                       text_color=None,
+                       back_color=None,
+                       dark=settings.dark_mode)
+
+            l2 = Label(text='Label 2',
+                       on_click=None,
+                       argument=None,
+                       disabled=False,
+                       text_weight=800,  # Tested
+                       height=50,  # Tested
+                       margins=10,  # Tested
+                       margin_top=None,
+                       icon='mdi-car-light-high',  # Tested
+                       icon_large=False,
+                       icon_small=False,
+                       icon_left=False,
+                       icon_color='red',  # Tested
+                       tooltip=None,
+                       text_color='white',  # Tested
+                       back_color='black',  # Tested
+                       dark=settings.dark_mode)
+
+            display(l1)
+            display(l2)
+
+        ipywidgets_vois_runner(kernel_code)
+
+        my_label_1_locator = page_session.get_by_text("Label 1", exact=False).locator('..').locator('..')
+        my_label_1_locator.wait_for()
+
+        my_label_2_locator = page_session.get_by_text("Label 2", exact=False).locator('..').locator('..')
+        my_label_2_locator.wait_for()
+
+        assert_vois_compare_image(image=my_label_1_locator.screenshot(animations='disabled'), postfix='1')
+        assert_vois_compare_image(image=my_label_2_locator.screenshot(animations='disabled'), postfix='2')
+
+        # my_color_1_locator.click()
+        #
+        # canvas_1_locator = page_session.locator('canvas').locator('..').locator('..')
+        #
+        # assert_vois_compare_image(image=canvas_1_locator.screenshot(animations='disabled'), postfix='1_canvas')
+        #
+        # # To remove overlay old canvas
+        # page_session.mouse.click(0, 0)
+        #
+        # my_color_2_locator = page_session.get_by_role("button", name='Color2')
+        # my_color_2_locator.wait_for()
+        #
+        # assert_vois_compare_image(image=my_color_2_locator.screenshot(animations='disabled'), postfix='2')
+        #
+        # my_color_2_locator.click()
+        #
+        # canvas_2_locator = page_session.locator("canvas").nth(1).locator('..').locator('..')
+
+        # assert_vois_compare_image(image=canvas_2_locator.screenshot(animations='disabled'), postfix='2_canvas')
